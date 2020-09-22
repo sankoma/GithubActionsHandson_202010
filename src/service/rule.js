009c752837d6dfba1bd6dc7cf7c259255d289d3c
chihiro <fujisawachihiro@gmail.com>
2020-09-22 19:01:14 +0900

    npm start 時の警告に対応

--- src/service/rule.js
+++ src/service/rule.js
@@ -13,26 +13,28 @@ export const play = (move, opponentMove) => {
   let result;
   switch (move) {
     case MOVE.rock:
-      if (opponentMove == MOVE.scissors) {
+      if (opponentMove === MOVE.scissors) {
         result = RESULT.win;
       } else {
         result = RESULT.lose;
       }
       break;
     case MOVE.scissors:
-      if (opponentMove == MOVE.paper) {
+      if (opponentMove === MOVE.paper) {
         result = RESULT.win;
       } else {
         result = RESULT.lose;
       }
       break;
     case MOVE.paper:
-      if (opponentMove == MOVE.rock) {
+      if (opponentMove === MOVE.rock) {
         result = RESULT.win;
       } else {
         result = RESULT.lose;
       }
       break;
+    default:
+      result = RESULT.draw;
   }
   return result;
 };
